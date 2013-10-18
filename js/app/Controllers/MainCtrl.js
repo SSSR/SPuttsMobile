5499150df37294c365323892ce6b00e0833db212
MikeDanilov <michaell.danilov@gmail.com>
2013-10-18 16:49:35 +0300

    try to get cross domain data in mobile and separate views

--- js/app/Controllers/MainCtrl.js
+++ js/app/Controllers/MainCtrl.js
@@ -1,3 +1,14 @@
-socialputts.controller('MainCtrl', function MainCtrl($scope) {
-    $scope.Hello = "Hello SP mobile app";
-})
+function MainCtrl($scope, $http){
+	var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D'http%3A%2F%2Fdailyjs.com%2Fatom.xml'%20and%20itemPath%3D'feed.entry'&format=json&diagnostics=true&callback=JSON_CALLBACK";
+	$http.jsonp(url).
+      success(function(data, status, headers, config) {
+        $scope.feed = {
+          items: data.query.results.entry
+        };
+      }).
+      error(function(data, status, headers, config) {
+        console.error('Error fetching feed:', data);
+      });
+	  
+	$scope.Hello = "Hello SP mobile app";
+}
--- js/app/app.js
+++ js/app/app.js
@@ -1,1 +1,8 @@
 var socialputts = angular.module('socialputts', ['ui.router']);
+
+socialputts.config(['$routeProvider', function($routeProvider){
+	$routeProvider
+		.when('/index', {templateUrl: 'partials/main.html', controller: MainCtrl})
+		.when('/signin', {templateUrl: 'partials/signIn.html', controller: MainCtrl})
+		.otherwise({ redirectTo: '/index'})
+}]);
