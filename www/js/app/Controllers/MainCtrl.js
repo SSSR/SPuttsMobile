c2485119ba8c9f820cc56e95be9cd429ee22a8e4
MikeDanilov <michaell.danilov@gmail.com>
2013-10-21 16:35:39 +0300

    get cross domain data in mobile

--- www/js/app/Controllers/MainCtrl.js
+++ www/js/app/Controllers/MainCtrl.js
@@ -1,14 +1,11 @@
 function MainCtrl($scope, $http){
-	var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D'http%3A%2F%2Fdailyjs.com%2Fatom.xml'%20and%20itemPath%3D'feed.entry'&format=json&diagnostics=true&callback=JSON_CALLBACK";
-	$http.jsonp(url).
-      success(function(data, status, headers, config) {
-        $scope.feed = {
-          items: data.query.results.entry
-        };
-      }).
-      error(function(data, status, headers, config) {
-        console.error('Error fetching feed:', data);
-      });
+	var url = "http://socialputts-test.azurewebsites.net/api/invitation/get?alt=json-in-script&callback=JSON_CALLBACK";
+	
+	$http.jsonp(url).success(function(data) {
+        $scope.invitations = data;
+      }).error(function(data){
+		console.log("error");
+	  });
 	  
 	$scope.Hello = "Hello SP mobile app";
 }
