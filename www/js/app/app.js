5baffcf6d9d5da619d108dc1d2d54d83b08ecc96
MikeDanilov <michaell.danilov@gmail.com>
2013-10-22 15:20:16 +0300

    commit

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -1,10 +1,6 @@
 var socialputts = angular.module('socialputts', ['socialputts.controllers', 'socialputts.services', 'ui.router']);
 
-socialputts
-.config(function ($compileProvider){
-    $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
-})
-.config(['$routeProvider', function($routeProvider){
+socialputts.config(['$routeProvider', function($routeProvider){
 	$routeProvider
 		.when('/index', {templateUrl: 'partials/main.html', controller: 'MainCtrl'})
 		.when('/signin', {templateUrl: 'partials/signIn.html', controller: 'MainCtrl'})
