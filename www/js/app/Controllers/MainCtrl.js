e74dc5f143e62d27635114081495a379de9ffdce
MikeDanilov <michaell.danilov@gmail.com>
2013-12-27 17:31:42 +0200

    some fix

--- www/js/app/Controllers/MainCtrl.js
+++ www/js/app/Controllers/MainCtrl.js
@@ -481,7 +481,7 @@
 .controller('OneClickDiscountCtrl', function ($scope, $http, $location, courseFinderService) {
     checkUserLogedOff($location, $scope);
 
-    var courseFormModel = {City: "", CountryId: "1", CourseName: "", Mileage: "30", NumberOfHoles: "0", Zip: "78664"};
+    var courseFormModel = {City: "", CountryId: "1", CourseName: "", Mileage: "30", NumberOfHoles: "0", Zip: $.jStorage.get('user').zip};
 
     courseFinderService.setObject(courseFormModel);
     courseFinderService.setCountry("United States");
